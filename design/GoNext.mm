98363df6cd8234b7ef46f294ee30ea38b7f6fdc0
House.Lee <hlee@vendasta.com>
2017-01-26 17:00:32 -0600

    Add mind map

---
+++ design/GoNext.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1485465470808" ID="ID_1337818339" MODIFIED="1485465478333" TEXT="GoNext">
+<node CREATED="1485466301689" ID="ID_1729102647" MODIFIED="1485469604923" POSITION="right" TEXT="Routines Pool"/>
+<node CREATED="1485469593812" ID="ID_325428311" MODIFIED="1485471577970" POSITION="right" TEXT="WorkFlow"/>
+<node CREATED="1485471578498" ID="ID_1527091100" MODIFIED="1485471583044" POSITION="right" TEXT="Worker"/>
+</node>
+</map>
